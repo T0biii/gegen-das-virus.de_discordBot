23aa178252462ab9aa4829bb1f5d0fd4227aaf74
Fr13dr1ch <friedrichlucas.99@gmail.com>
2020-03-18 22:39:57 +0100

    Optimize Botjs

--- bot.js
+++ bot.js
@@ -1,9 +1,11 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 var fs = require("fs");
+const adminchannel;
 
 client.on("ready", () => {
   console.log("Discord Bot started!");
+  adminchannel = client.channels.cache.get("689949212011528262");
 });
 
 client.on("message", msg => {
@@ -21,7 +23,7 @@ client.on("guildMemberAdd", member => {
     }
     let languageJSON = JSON.parse(fs.readFileSync("./assets/i18n/" + localeCode + ".json"));
     member.send({embed: {color: 3447003, fields: languageJSON.welcome}});
-
+    adminchannel.send("Ein neuer Benutzer ist gejoint " + member.nickname);
   } catch (e) {
     console.error(e);
   }
